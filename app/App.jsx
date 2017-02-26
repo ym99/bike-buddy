0fddba644fab9b74c07f2a74589e593273cce96b
ym99 <yuriy.myshkevych@gmail.com>
2017-02-26 17:39:29 -0500

    progress

--- app/App.jsx
+++ app/App.jsx
@@ -1,8 +1,44 @@
 import React from 'react';
 import Map from './Map';
+import { testData } from './testData';
 
 export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      history: [],
+    };
+  }
+
+  componentDidMount() {
+    this.timerHandle = setInterval(() => {
+      if (testData.length > 0) {
+        this.setState((prevState) => {
+          const history = [...prevState.history];
+          history.push({
+            time: new Date(),
+            latLng: testData.splice(0, 1)[0],
+          });
+
+          return {
+            history,
+          };
+        });
+      }
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerHandle);
+  }
+
   render() {
-    return <Map />;
+    return (
+      <Map
+        history={this.state.history}
+      />
+    );
   }
 }
--- app/Map.jsx
+++ app/Map.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 
 export default class Map extends React.Component {
+  static propTypes = {
+    history: React.PropTypes.arrayOf(
+      React.PropTypes.shape({
+        time: React.PropTypes.instanceOf(Date).isRequired,
+        latLng: React.PropTypes.shape({
+          lat: React.PropTypes.number.isRequired,
+          lng: React.PropTypes.number.isRequired,
+        }),
+      }),
+    ),
+  };
+
   constructor(props) {
     super(props);
 
     this.apiKey = 'AIzaSyCwddszOGNsmeumxZC24JkhvzM1nZogpk4';
     this.initMap = this.initMap.bind(this);
+    this.path = this.path.bind(this);
   }
 
   componentDidMount() {
@@ -18,37 +31,44 @@ export default class Map extends React.Component {
     window.document.body.appendChild(mapsScript);
   }
 
+  componentDidUpdate() {
+    this.polyline.setPath(this.path());
+  }
+
   initMap() {
     const map = new google.maps.Map(
       this.mapObj, {
         center: { lat: 38.8648, lng: -77.0380 },
         clickableIcons: false,
         disableDefaultUI: true,
-        disableDoubleClickZoom: false,
-        draggable: false,
         mapTypeId: google.maps.MapTypeId.SATELLITE,
         scrollwheel: true,
         zoom: 17,
       });
 
+    // map.addListener('click', (event) => {
+    //   console.log(`{ lat: ${event.latLng.lat()}, lng: ${event.latLng.lng()} },`);
+    // });
+
     const bikeLayer = new google.maps.BicyclingLayer();
     bikeLayer.setMap(map);
 
-    const points = [
-      { lat: 38.8648, lng: -77.0380 },
-      { lat: 38.7854, lng: -77.0630 },
-      { lat: 38.7293, lng: -77.1074 },
-    ];
-
-    const polyline = new google.maps.Polyline({
-      path: points,
+    this.polyline = new google.maps.Polyline({
+      path: this.path(),
       geodesic: true,
       strokeColor: 'yellow',
       strokeOpacity: 1.0,
       strokeWeight: 1,
     });
 
-    polyline.setMap(map);
+    this.polyline.setMap(map);
+  }
+
+  path() {
+    return this.props.history.map(item => ({
+      lat: item.latLng.lat,
+      lng: item.latLng.lng,
+    }));
   }
 
   render() {
